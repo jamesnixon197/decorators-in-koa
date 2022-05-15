7a43ae8eb5d5a43be800cc4c2be26817e00b8dad
James Nixon <james.nixon@truepill.com>
2022-05-15 17:02:38 +0100

    Deploy to npm instead of github registry

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,7 @@ module.exports = {
   repositoryUrl: 'https://github.com/jamesnixon197/koa-decorator-router.git',
   debug: true,
   plugins: [
-    '@semantic-release/commit-analyzer',
+    // '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     [
