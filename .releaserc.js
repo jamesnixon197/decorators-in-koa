3489cd06151f44c438be7a8c2cccda5f01cb4973
James Nixon <james.nixon@truepill.com>
2022-05-15 18:37:21 +0100

    Update releaserc rules

--- .releaserc.js
+++ .releaserc.js
@@ -2,7 +2,11 @@ module.exports = {
   repositoryUrl: 'https://github.com/jamesnixon197/koa-and-decorators.git',
   debug: true,
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      "releaseRules": [
+        {"type": "docs", "scope":"README", "release": "patch"},
+      ]
+    }],
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     [
